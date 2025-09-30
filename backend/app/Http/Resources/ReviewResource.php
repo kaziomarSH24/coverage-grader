ea711d163f35378bb6d85e32a4f9b722ba6046b8
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-09-30 14:51:28 +0600

    review vote done

--- backend/app/Http/Resources/ReviewResource.php
+++ backend/app/Http/Resources/ReviewResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Resources\Admin\InsuranceProviderResource;
+use App\Http\Resources\InsuranceProviderResource;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
